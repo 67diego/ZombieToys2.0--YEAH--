51bc079cac6f7bb3ce34c26b0b353bd37fdb274a
67diego <dmunoz024@gmail.com>
2016-12-14 09:13:28 -0600

    UploadFromOldLaptop(BecauseIAmChangingLaptop(Duh))
    Oh, there is a description box...

---
+++ Assets/Scripts/RotateLigght.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateLigght : MonoBehaviour {
+	public float speedo;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.forward*speedo,Space.World);
+	}
+}
